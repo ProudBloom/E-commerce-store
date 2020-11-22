0edb2ea4e0ae3a966186b93a2bb142b8f7c92b8c
ProudBloom <jakub-sztompka@wp.pl>
2020-11-22 01:10:13 +0100

    Dynamic data introduced in index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import data from './data'
+
 function App() {
   return (
     <div>
       <header>
-            <a href="index.html" id="logo"><img src="../assets/img/Logo.svg" alt="Logo" /></a>
+            <a href="/index.html" id="logo"><img src="../assets/img/Logo.svg" alt="Logo" /></a>
             <div className="header__menu">
                 <a href="#">Mens</a>
                 <a href="#">Ladies</a>
@@ -20,7 +23,7 @@ function App() {
                 </div>
                 <div className="menu__wrapper">
                     <a href="#">
-                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path d="M29.8269 6.26784C29.6551 6.02172 29.3788 5.86959 29.0786 5.85677L10.4694 5.05462C9.93543 5.03126 9.48924 5.44348 9.46641 5.97493C9.44372 6.50617 9.85527 6.95505 10.3866 6.97787L27.736 7.72577L24.3248 18.3691H9.14429L6.40171 3.43597C6.34143 3.10868 6.11672 2.83564 5.80682 2.71405L1.31494 0.949381C0.819879 0.755558 0.261239 0.998726 0.0668122 1.49318C-0.127278 1.98791 0.115822 2.54689 0.610615 2.74131L4.60461 4.31035L7.3956 19.5056C7.47972 19.9624 7.87784 20.2942 8.34249 20.2942H8.80546L7.74827 23.2307C7.65978 23.4767 7.69623 23.7499 7.84682 23.9636C7.99721 24.1774 8.24178 24.3045 8.50288 24.3045H9.24439C8.78491 24.8159 8.50288 25.4892 8.50288 26.2296C8.50288 27.822 9.79854 29.1173 11.3905 29.1173C12.9825 29.1173 14.2782 27.822 14.2782 26.2296C14.2782 25.4893 13.9962 24.8159 13.5367 24.3045H19.8326C19.3729 24.8159 19.0909 25.4892 19.0909 26.2296C19.0909 27.822 20.3862 29.1173 21.9786 29.1173C23.571 29.1173 24.8662 27.822 24.8662 26.2296C24.8662 25.4893 24.5843 24.8159 24.1249 24.3045H25.0267C25.4698 24.3045 25.8288 23.9454 25.8288 23.5024C25.8288 23.0593 25.4697 22.7003 25.0267 22.7003H9.64419L10.5104 20.2939H25.0266C25.4452 20.2939 25.8156 20.0235 25.9431 19.6253L29.9538 7.11194C30.0459 6.82635 29.9986 6.5141 29.8269 6.26784ZM11.3906 27.5133C10.6828 27.5133 10.1072 26.9378 10.1072 26.23C10.1072 25.5222 10.6828 24.9465 11.3906 24.9465C12.0984 24.9465 12.674 25.5222 12.674 26.23C12.674 26.9378 12.0984 27.5133 11.3906 27.5133ZM21.9786 27.5133C21.2708 27.5133 20.6952 26.9378 20.6952 26.23C20.6952 25.5222 21.2708 24.9465 21.9786 24.9465C22.6864 24.9465 23.262 25.5222 23.262 26.23C23.262 26.9378 22.6864 27.5133 21.9786 27.5133Z" fill="white"/></g><defs><clipPath id="clip0"><rect width="30" height="30" fill="white"/></clipPath></defs></svg> 
+                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0)"><path d="M29.8269 6.26784C29.6551 6.02172 29.3788 5.86959 29.0786 5.85677L10.4694 5.05462C9.93543 5.03126 9.48924 5.44348 9.46641 5.97493C9.44372 6.50617 9.85527 6.95505 10.3866 6.97787L27.736 7.72577L24.3248 18.3691H9.14429L6.40171 3.43597C6.34143 3.10868 6.11672 2.83564 5.80682 2.71405L1.31494 0.949381C0.819879 0.755558 0.261239 0.998726 0.0668122 1.49318C-0.127278 1.98791 0.115822 2.54689 0.610615 2.74131L4.60461 4.31035L7.3956 19.5056C7.47972 19.9624 7.87784 20.2942 8.34249 20.2942H8.80546L7.74827 23.2307C7.65978 23.4767 7.69623 23.7499 7.84682 23.9636C7.99721 24.1774 8.24178 24.3045 8.50288 24.3045H9.24439C8.78491 24.8159 8.50288 25.4892 8.50288 26.2296C8.50288 27.822 9.79854 29.1173 11.3905 29.1173C12.9825 29.1173 14.2782 27.822 14.2782 26.2296C14.2782 25.4893 13.9962 24.8159 13.5367 24.3045H19.8326C19.3729 24.8159 19.0909 25.4892 19.0909 26.2296C19.0909 27.822 20.3862 29.1173 21.9786 29.1173C23.571 29.1173 24.8662 27.822 24.8662 26.2296C24.8662 25.4893 24.5843 24.8159 24.1249 24.3045H25.0267C25.4698 24.3045 25.8288 23.9454 25.8288 23.5024C25.8288 23.0593 25.4697 22.7003 25.0267 22.7003H9.64419L10.5104 20.2939H25.0266C25.4452 20.2939 25.8156 20.0235 25.9431 19.6253L29.9538 7.11194C30.0459 6.82635 29.9986 6.5141 29.8269 6.26784ZM11.3906 27.5133C10.6828 27.5133 10.1072 26.9378 10.1072 26.23C10.1072 25.5222 10.6828 24.9465 11.3906 24.9465C12.0984 24.9465 12.674 25.5222 12.674 26.23C12.674 26.9378 12.0984 27.5133 11.3906 27.5133ZM21.9786 27.5133C21.2708 27.5133 20.6952 26.9378 20.6952 26.23C20.6952 25.5222 21.2708 24.9465 21.9786 24.9465C22.6864 24.9465 23.262 25.5222 23.262 26.23C23.262 26.9378 22.6864 27.5133 21.9786 27.5133Z" fill="white"/></g><defs><clipPath id="clip0"><rect width="30" height="30" fill="white"/></clipPath></defs></svg> 
                     </a>
                     <p>Shopping Cart</p>
                 </div>
@@ -29,29 +32,34 @@ function App() {
 
         <main>
             <div className="card__wrapper">
-                <div className="card">
-                    <div className="card__stars">
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star" aria-hidden="true"></i>
-                        <i className="fa fa-star-half" aria-hidden="true"></i>
+            {
+                data.products.map(product => 
+                (
+                    <div key={product._id} className="card">
+                        <div className="card__stars">
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star" aria-hidden="true"></i>
+                            <i className="fa fa-star-half" aria-hidden="true"></i>
+                        </div>
+                        <h4>{product.category}</h4>
+                        <h1>{product.name}</h1>
+                        <h1 className="card__price">{product.price}</h1>
+                        <img className="card__image" src={product.image} alt="nike predator shoe" />
+                        <div className="card__buttons">
+                            <button className="card__add-to-cart">
+                                <img src="../assets/icons/shopping_cart-small.svg" alt="shopping cart" />
+                                <p>Add to cart</p>
+                            </button>
+                            <button className="card__details">
+                                <img src="../assets/icons/details-small.svg" alt="magnifier" />
+                                <a href={`/product/${product._id}`}><p>Details</p></a>
+                            </button>
+                        </div>
                     </div>
-                    <h4>Sneakers</h4>
-                    <h1>Nike predator</h1>
-                    <h1 className="card__price">224.99$</h1>
-                    <img className="card__image" src="../assets/img/shoes1.png" alt="nike predator shoe" />
-                    <div className="card__buttons">
-                        <button className="card__add-to-cart">
-                            <img src="../assets/icons/shopping_cart-small.svg" alt="shopping cart" />
-                            <p>Add to cart</p>
-                        </button>
-                        <button className="card__details">
-                            <img src="../assets/icons/details-small.svg" alt="magnifier" />
-                            <p>Details</p>
-                        </button>
-                    </div>
-                </div>
+                ))
+            }
             </div>
         </main>
 
---
+++ frontend/src/data.js
@@ -0,0 +1,63 @@
+const data = 
+{
+    products:
+    [
+        {
+            _id: 1,
+            name: 'Nike Predator',
+            category: 'Sneakers',
+            image: '/assets/img/shoes1.png',
+            price: '224.99zł',
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Red nike sneakers made of natural materials',
+        },
+        {
+            _id: 2,
+            name: 'Nike React Miler',
+            category: 'Sneakers',
+            image: '/assets/img/shoes2.png',
+            price: '215zł',
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 14,
+            description: 'Sportswear designed to be lightweight and ultra-durable',
+        },
+        {
+            _id: 3,
+            name: 'Tom Tailor Shoes',
+            category: 'Casual',
+            image: '/assets/img/shoes3.png',
+            price: '199.99zł',
+            brand: 'Tom Tailor',
+            rating: 3.9,
+            numReviews: 21,
+            description: 'Casual leather shoes for daily use',
+        },
+        {
+            _id: 4,
+            name: 'Nike Air Max 2090',
+            category: 'Sneakers',
+            image: '/assets/img/shoes4.png',
+            price: '499.99zł',
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 36,
+            description: 'Newest edition of Air Max series designed to perform the best possible',
+        },
+        {
+            _id: 5,
+            name: 'Adidas Superstar',
+            category: 'Trainers',
+            image: '/assets/img/shoes5.png',
+            price: '369.49zł',
+            brand: 'Adidas',
+            rating: 4.7,
+            numReviews: 18,
+            description: 'Adidas shoes perfect for common daily use',
+        }
+    ]
+}
+
+export default data;

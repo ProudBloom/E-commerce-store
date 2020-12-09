c6c487f38a3eb2bf1c3455ff210bea496a1a8efb
Jakub Sztompka <jakub.sztompka@int.pl>
2020-12-09 23:13:37 +0100

    Product description added (need future modifications)

--- frontend/src/views/ProductDetails/ProductDetails.js
+++ frontend/src/views/ProductDetails/ProductDetails.js
@@ -33,13 +33,32 @@ export default function ProductDetails() {
                     </h1>
                     <h1 className='details__name'>{product.name}</h1>
                     <h2 className='details__price'>{product.price}</h2>
+                    <p>{product.inStock > 0 ? (<span className="details__in-stock">In stock</span>) : (<span className="details__not-in-stock">Not available</span>)}</p>
                     <button className="details__add-to-cart-btn"><span>Add to cart</span></button>
                 </div>
                 <img src={product.image}></img>
                 <a className="more-details" href="#prod-description"><i className="fa fa-angle-down" /></a>
             </div>
-            <div id="prod-description">
-                <p>{product.description}</p>
+            {/* TODO: apply more dteailed description as object and style correspondingly (description: {someInfo: 'info1', someOther: 'info2'})*/}
+            <div className="description-wrapper">
+                <div className="reviews">
+                {/* TODO: implement user reviews (UserName: 'text of review', rating)*/}
+                {/* TODO: use BEM convention here*/}
+                    <ul>
+                        <li>Review</li>
+                        <li>Review</li>
+                        <li>Review</li>
+                        <li>Review</li>
+                        <li>Review</li>
+                        <li>Review</li>
+                        <li>Review</li>
+                        <li>Review</li>
+                    </ul>
+                </div>
+                <div id="prod-description">
+                    <h2>Description</h2>
+                    <p>{product.description}</p>
+                </div>
             </div>
         </div>
     )

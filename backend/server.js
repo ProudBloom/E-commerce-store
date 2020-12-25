94af309dec33e9d8414c622b4cadb0c1995759ad
ProudBloom <jakub-sztompka@wp.pl>
2020-12-25 21:32:31 +0100

    New button style + home screen modified

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,15 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id == req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
+  });
+
 app.listen(SERVER_PORT, () => {
     console.log('Server running on http://localhost:' + SERVER_PORT);
 });
--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -3,7 +3,13 @@ import './HomePage.scss'
 
 export default function HomePage() {
     return (
-        <div className="home__background"> 
+        <div className="home__background">
+            <div className="home__heading">
+                <h1>Modern e-commerce shoe store</h1>
+                <hr />
+                <h3>You can buy any shoes that will match your needs. Anytime. Anywhere.</h3>
+                <a href="/products-list" className="heading__button"><span>See our products<i class="fa fa-angle-right"></i></span></a>
+            </div>
             <p className="home__copyright">© E-Shoerr 2020</p>
         </div>
     )
